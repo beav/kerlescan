205e7086f6f7835435396a814f2461dda53b6c31
Chris Duryee <cduryee@redhat.com>
2020-02-10 11:07:38 -0500

    feature: add BYPASS_RBAC env var
    If `BYPASS_RBAC` is set to `BYPASS`, the rbac check will always pass.
    This is useful for enabling/disabling the feature.

--- kerlescan/config.py
+++ kerlescan/config.py
@@ -12,3 +12,5 @@ hsp_svc_hostname = os.getenv("HSP_SVC_URL", "http://hsp_svc_url_is_not_set")
 prometheus_multiproc_dir = os.getenv("prometheus_multiproc_dir", None)
 
 path_prefix = os.getenv("PATH_PREFIX", "/api/")
+
+bypass_rbac = os.getenv("BYPASS_RBAC", None)
